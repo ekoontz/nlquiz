324fbdb09cad4ef2eaf32e1fdc4d1231628eb426
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-02 00:26:01 +0100

    Revert "move deserialize-lexicon definition to the bottom"
    This reverts commit 05d2a4be07ec60554b1ddc0fe3e7bab144c9760d.

--- src/cljs/babylonui/language.cljs
+++ src/cljs/babylonui/language.cljs
@@ -7,7 +7,13 @@
    [cljslog.core :as log]
    [dag_unify.core :as u]))
 
-(declare deserialize-lexicon)
+(defn deserialize-lexicon [map-with-serializations]
+  (zipmap
+   (keys map-with-serializations)
+   (map (fn [serializations]
+          (vec (map dag_unify.serialization/deserialize
+                    serializations)))
+        (vals map-with-serializations))))
 
 (def en-lexicon (deserialize-lexicon (en/read-compiled-lexicon)))
 (def nl-lexicon (deserialize-lexicon (nl/read-compiled-lexicon)))
@@ -21,12 +27,3 @@
     (log/info (str "showing noun-type rule: " (u/get-in rule [:rule])))
     rule))
 
-
-;; auxiliary functions:
-(defn deserialize-lexicon [map-with-serializations]
-  (zipmap
-   (keys map-with-serializations)
-   (map (fn [serializations]
-          (vec (map dag_unify.serialization/deserialize
-                    serializations)))
-        (vals map-with-serializations))))
