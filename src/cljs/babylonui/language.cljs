9b3c9ec945704774f6561e0f9a830c9e57fa8968
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-04 00:18:48 +0100

    cleanup

--- src/cljs/babylonui/language.cljs
+++ src/cljs/babylonui/language.cljs
@@ -15,34 +15,3 @@
           (vec (map dag_unify.serialization/deserialize
                     serializations)))
         (vals map-with-serializations))))
-
-(def en-lexicon (deserialize-lexicon (en/read-compiled-lexicon)))
-
-(def nl-grammar (->> (nl/read-compiled-grammar)
-                     (map dag_unify.serialization/deserialize)))
-(def nl-lexicon (deserialize-lexicon (nl/read-compiled-lexicon)))
-
-(defn generate-a-np [grammar lexicon index-fn]                            
-  (let [rule
-        (u/unify
-         (first (shuffle (filter #(and
-                                    (= :noun (u/get-in % [:cat]))
-                                    (empty? (u/get-in % [:subcat])))
-                                 grammar)))
-         {:head {:phrasal false}
-          :comp {:phrasal false}
-          :babylon.generate/started? true})
-        syntax-tree nl/syntax-tree
-        morph nl/morph]
-    (log/debug (str "showing noun-type rule: " (u/get-in rule [:rule])))
-    (let [phrase
-          (first
-           (-> rule
-               (g/add grammar index-fn syntax-tree)
-               (g/add grammar index-fn syntax-tree)))]
-      (log/debug (str "after add: " (syntax-tree phrase)))
-      (log/debug (str "head: " (u/get-in phrase [:head])))
-      {:tree (syntax-tree phrase)
-       :rule (u/get-in phrase [:rule])
-       :surface (morph phrase)})))
-
