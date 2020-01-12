c8766b14bb42848f841844ea1be097399c6705d6
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-12 02:25:59 +0100

    ~lexeme-map: exclude lexemes that are [:exception]s

--- src/cljs/babylonui/nederlands.cljs
+++ src/cljs/babylonui/nederlands.cljs
@@ -91,18 +91,23 @@
         @lexicon-atom)
     @lexicon-atom))
 
+;; note that we exclude [:exception]s from the lexemes that we use for
+;; generation since they are only to be used for parsing.
+;; TODO: this is duplicated in babylon/nederlands.cljc (see def verb-lexicon).
 (defn lexeme-map []
   (if (nil? @lexeme-map-atom)
     (do (swap! lexeme-map-atom
                (fn []
                  {:verb (->> (lexicon)
-                             (filter #(= :verb (u/get-in % [:cat]))))
+                             (filter #(= :verb (u/get-in % [:cat])))
+                             (filter #(not (u/get-in % [:exception]))))
                   :det (->> (lexicon)
                             (filter #(= :det (u/get-in % [:cat]))))
                   :intensifier (->> (lexicon)
                                     (filter #(= :intensifier (u/get-in % [:cat]))))
                   :noun (->> (lexicon)
-                             (filter #(= :noun (u/get-in % [:cat]))))
+                             (filter #(= :noun (u/get-in % [:cat])))
+                             (filter #(not (u/get-in % [:exception]))))
                   :top (lexicon)
                   :adjective (->> (lexicon)                                                          
                                   (filter #(= :adjective (u/get-in % [:cat]))))})))
