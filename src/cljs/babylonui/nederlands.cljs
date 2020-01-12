0845c04e9faa33776b9d36225f87ad68cb02b8ee
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-12 16:15:12 +0100

    move index-fn to babylon.nederlands.lexicon

--- src/cljs/babylonui/nederlands.cljs
+++ src/cljs/babylonui/nederlands.cljs
@@ -19,7 +19,7 @@
           (g/generate spec
                       (grammar)
                       (fn [spec]
-                        (shuffle (index-fn spec)))
+                        (shuffle (l/index-fn spec)))
                       syntax-tree)
           (catch js/Error e
             (cond
@@ -78,39 +78,5 @@
   (or @expressions-atom
       (do (swap! expressions-atom (fn [] (nl/read-expressions))))))
 
-;; note that we exclude [:exception]s from the lexemes that we use for
-;; generation since they are only to be used for parsing.
-;; TODO: this is duplicated in babylon/nederlands.cljc (see def verb-lexicon).
-(defn lexeme-map []
-  (if (nil? @l/lexeme-map-atom)
-    (do (swap! l/lexeme-map-atom
-               (fn []
-                 {:verb (->> (l/lexicon)
-                             (filter #(= :verb (u/get-in % [:cat])))
-                             (filter #(not (u/get-in % [:exception]))))
-                  :det (->> (l/lexicon)
-                            (filter #(= :det (u/get-in % [:cat]))))
-                  :intensifier (->> (l/lexicon)
-                                    (filter #(= :intensifier (u/get-in % [:cat]))))
-                  :noun (->> (l/lexicon)
-                             (filter #(= :noun (u/get-in % [:cat])))
-                             (filter #(not (u/get-in % [:exception]))))
-                  :top (l/lexicon)
-                  :adjective (->> (l/lexicon)                                                          
-                                  (filter #(= :adjective (u/get-in % [:cat]))))})))
-    @l/lexeme-map-atom))
-
 (defn syntax-tree [tree]
   (s/syntax-tree tree (morphology)))
-
-(defn index-fn [spec]
-  ;; for now a somewhat bad index function: simply returns
-  ;; lexemes which match the spec's :cat, or, if the :cat isn't
-  ;; defined, just return all the lexemes.
-  (log/debug (str "looking for key: " (u/get-in spec [:cat] ::none)))
-  (let [result (get (lexeme-map) (u/get-in spec [:cat] :top) nil)]
-    (if (not (nil? result))
-        (shuffle result)
-        (do
-          (log/info (str "no entry from cat: " (u/get-in spec [:cat] ::none) " in lexeme-map: returning all lexemes."))
-          (l/lexicon)))))
