a72aa150f0d7beeecbb6ee92c2b515feaf3d32da
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-20 22:38:23 +0100

    whitespace and plus more stuff, all working

--- src/cljs/babylonui/core.cljs
+++ src/cljs/babylonui/core.cljs
@@ -3,6 +3,7 @@
    [accountant.core :as accountant]
    [babylon.english :as en]
    [babylon.nederlands :as nl]
+   [babylon.translate :as tr]
    [clerk.core :as clerk]
    [cljslog.core :as log]
    [clojure.string :as string]
@@ -43,111 +44,124 @@
   (apply swap! app-state update-in [:expressions] f args))
 
 (defn source-spec [expression]
-  {:sem (u/get-in expression [:sem])
-   :phrasal (u/get-in expression [:phrasal])
-   :head {:phrasal (u/get-in expression [:head :phrasal] :top)}
-   :subcat (u/get-in expression [:subcat])
-   :agr (u/get-in expression [:agr])
-   :modal (u/get-in expression [:modal] false)
-   :comp {:pronoun (u/get-in expression [:comp :pronoun] :top)
-          :phrasal (u/get-in expression [:comp :phrasal] :top)}
-   :cat (u/get-in expression [:cat])})
+  (if true
+    (tr/nl-to-en-spec expression)
+    {:sem (u/get-in expression [:sem])
+     :phrasal (u/get-in expression [:phrasal])
+     :head {:phrasal (u/get-in expression [:head :phrasal] :top)}
+     :subcat (u/get-in expression [:subcat])
+     :agr (u/get-in expression [:agr])
+     :modal (u/get-in expression [:modal] false)
+     :comp {:pronoun (u/get-in expression [:comp :pronoun] :top)
+            :phrasal (u/get-in expression [:comp :phrasal] :top)}
+     :cat (u/get-in expression [:cat])}))
 
-(defn contact [c]
+(defn expression [c]
   (log/info (str "generating nl.."))
-  (let [expression (nl/generate @expression-specification-atom)]
+  (let [target-spec (u/unify @expression-specification-atom
+                             {:cat :noun})
+        expression (nl/generate target-spec)]
+    (log/info (str "target expression: " (nl/morph expression)))
     [:div.row
      [:div.expression 
       [:span (nl/morph expression)]]
-     (log/info (str "generating en.."))
-     [:div.expression 
-      [:span (en/morph (en/generate (source-spec expression)))]]]))
+     [:div.expression
+      (let [source-spec (source-spec expression)]
+        (log/info (str "generating source expression from spec: " (u/strip-refs source-spec)))
+        (let [source-expression (en/generate source-spec)]
+          (log/info (str (nl/morph expression) " => " (en/morph source-expression)))
+          [:span (en/morph source-expression)]))]]))
 
-(defn contact-list []
+(defn expression-list []
   [:div
    (for [c (:expressions @app-state)]
-     [contact c])])
+     [expression c])])
 
-(defn add-contact! [c]
-  (update-expressions! #(conj % c)))
+(defn add-expression! [c]
+  (update-expressions!
+   (fn [existing-expressions]
+     (log/info (str "length of existing expressions: " (count existing-expressions)))
+     (if (> (count existing-expressions) 5)
+       (cons c (butlast existing-expressions))
+       (cons c existing-expressions)))))
 
 (defn noop [arg])
 
 (set! (.-onload js/window) 
-      (fn []
-        (noop 42)))
+(fn []
+  (noop 42)))
 
 (declare show-expressions-dropdown)
 
 (def next-key (atom 0))
 (defn get-next-key []
-  (let [next-value @next-key]
-    (swap! next-key (fn [] (+ 1 @next-key)))
-    next-value))
+(let [next-value @next-key]
+  (swap! next-key (fn [] (+ 1 @next-key)))
+  next-value))
 
 (defn home-page []
-  (fn []
-    [:div.main
+(fn []
+  [:div.main
 
-     [:div
-      [:input {:type "button" :value "Generate NL phrase"
-               :on-click #(add-contact! {:key (get-next-key)})}]
-      [show-expressions-dropdown]]
+   [:div
+    [:input {:type "button" :value "Generate NL phrase"
+             :on-click #(add-expression! {:key (get-next-key)})}]
+    [show-expressions-dropdown]]
 
-     [:div.debugpanel
-      [:div
-       (str @expression-specification-atom)]
+   [:div.debugpanel
+    [:div
+     (str @expression-specification-atom)]
 
-      [:div
-       (str @semantics-atom)]]
+    [:div
+     (str @semantics-atom)]]
 
-     [:div {:style {:width "100%" :float "left" :height "90%" :border "0px dashed blue"
-                    :overflow "scroll"}}
-      [contact-list]]]))
+   [:div {:style {:width "100%" :float "left" :height "90%" :border "0px dashed blue"
+                  :overflow "scroll"}}
+    [expression-list]]]))
 
 (defn show-expressions-dropdown []
-  [:div {:style {:float "left" :border "0px dashed blue"}}
-   [:select {:id "expressionchooser"
-             :on-change #(do
-                           (swap! expression-specification-atom
-                                  (fn [x]
-                                    (nth nl/expressions
-                                         (js/parseInt (dommy/value (dommy/sel1 :#expressionchooser))))))                   
-                           (add-contact! {}))}
-    (map (fn [item-id]
-           (let [expression (nth nl/expressions item-id)]
-             [:option {:name item-id
-                       :value item-id
-                       :key (str "item-" item-id)}
-              (:note expression)]))
-         (range 0 (count nl/expressions)))]])
+[:div {:style {:float "left" :border "0px dashed blue"}}
+ [:select {:id "expressionchooser"
+           :on-change #(do
+                         (swap! expression-specification-atom
+                                (fn [x]
+                                  (nth nl/expressions
+                                       (js/parseInt (dommy/value (dommy/sel1 :#expressionchooser))))))                   
+                         (add-expression! {}))}
+  (map (fn [item-id]
+         (let [expression (nth nl/expressions item-id)]
+           [:option {:name item-id
+                     :value item-id
+                     :key (str "item-" item-id)}
+            (:note expression)]))
+       (range 0 (count nl/expressions)))]])
 
 (defn about-page []
-  (fn [] [:span.main
-          [:h1 "About babylon UI"]]))
+(fn [] [:span.main
+        [:h1 "About babylon UI"]]))
 
 ;; -------------------------
 ;; Translate routes -> page components
 
 (defn page-for [route]
-  (case route
-    :index #'home-page
-    :about #'about-page))
+(case route
+  :index #'home-page
+  :about #'about-page))
 
 ;; -------------------------
 ;; Page mounting component
 
 (defn current-page []
-  (fn []
-    (let [page (:current-page (session/get :route))]
-      [:div
-       [:header
-        [:p [:a {:href (path-for :index)} "Home"] " | "
-         [:a {:href (path-for :about)} "About babylon UI"]]]
-       [page]
-       [:footer
-        [:p "Babylon UI was generated by the "
-         [:a {:href "https://github.com/reagent-project/reagent-template"} "Reagent Template"] "."]]])))
+(fn []
+  (let [page (:current-page (session/get :route))]
+    [:div
+     [:header
+      [:p [:a {:href (path-for :index)} "Home"] " | "
+       [:a {:href (path-for :about)} "About babylon UI"]]]
+     [page]
+     [:footer
+      [:p "Babylon UI was generated by the "
+       [:a {:href "https://github.com/reagent-project/reagent-template"} "Reagent Template"] "."]]])))
 
 ;; -------------------------
 ;; Initialize app
