18c123778466f1b9a78af415e4e3e86d8e1ea2d6
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-25 10:24:36 +0100

    +atom-input;shared-state

--- src/cljs/babylonui/core.cljs
+++ src/cljs/babylonui/core.cljs
@@ -101,6 +101,18 @@
     (swap! next-key (fn [] (+ 1 @next-key)))
     next-value))
 
+(defn atom-input [value]
+  [:input {:type "text"
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn shared-state []
+  (let [val (r/atom "foo")]
+    (fn []
+      [:div
+       [:p "The value is now: " @val]
+       [:p "Change it here: " [atom-input val]]])))
+
 (defn home-page []
   (fn []
     [:div.main
