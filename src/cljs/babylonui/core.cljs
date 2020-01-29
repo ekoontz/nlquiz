96a287e3254aafe189299858a4a4dba9d0c1b911
Eugene Koontz <ekoontz@hiro-tan.org>
2020-01-29 22:31:50 +0100

    use :menuable?=true to filter out broken or overly-slow expressions

--- src/cljs/babylonui/core.cljs
+++ src/cljs/babylonui/core.cljs
@@ -148,19 +148,22 @@
      ]))
 
 (defn show-expressions-dropdown []
-[:div {:style {:float "left" :border "0px dashed blue"}}
- [:select {:id "expressionchooser"
-           :on-change #(reset! expression-specification-atom
-                               (nth nl/expressions
-                                    (js/parseInt
-                                     (dommy/value (dommy/sel1 :#expressionchooser)))))}
-  (map (fn [item-id]
-         (let [expression (nth nl/expressions item-id)]
-           [:option {:name item-id
-                     :value item-id
-                     :key (str "item-" item-id)}
-            (:note expression)]))
-       (range 0 (count nl/expressions)))]])
+  (let [show-these-expressions
+        (filter #(= true (u/get-in % [:menuable?] true))
+                nl/expressions)]
+    [:div {:style {:float "left" :border "0px dashed blue"}}
+     [:select {:id "expressionchooser"
+               :on-change #(reset! expression-specification-atom
+                                   (nth show-these-expressions
+                                        (js/parseInt
+                                         (dommy/value (dommy/sel1 :#expressionchooser)))))}
+      (map (fn [item-id]
+             (let [expression (nth show-these-expressions item-id)]
+               [:option {:name item-id
+                         :value item-id
+                         :key (str "item-" item-id)}
+                (:note expression)]))
+           (range 0 (count show-these-expressions)))]]))
 
 (defn about-page []
 (fn [] [:span.main
