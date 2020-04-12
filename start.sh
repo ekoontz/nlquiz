c58a4cd76884d040354896dce0ea4e274e60936d
Eugene Koontz <ekoontz@hiro-tan.org>
2020-04-13 01:58:54 +0200

    fix apparent problems when loading app.js than intended app-optimized.js

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#(cd ../babylon ; make clean; make compile; lein install); 
-rm target/cljsbuild/public/js/app-optimized.js
+(cd ../babylon ; make clean; make compile; lein install);
+rm target/cljsbuild/public/js/app.js target/cljsbuild/public/js/app-optimized.js
 lein cljsbuild once min
 lein figwheel
