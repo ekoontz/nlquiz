3afa6b37b1b3002008d019ff2c67962cce402993
Eugene Koontz <ekoontz@hiro-tan.org>
2020-04-13 01:57:44 +0200

    use latest dag_unify release

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
                  [cljs-http "0.1.46"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.5.0"]
-                 [dag_unify "1.9.1-SNAPSHOT"]
+                 [dag_unify "1.9.1"]
                  [babylon "0.0.1-SNAPSHOT"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-defaults "0.3.2"]
