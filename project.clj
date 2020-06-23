aa9a34fb201a6edfe82099d01b2112dda64bdc32
Eugene Koontz <ekoontz@hiro-tan.org>
2020-06-23 21:03:35 +0200

    update url to live demo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nlquiz "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://hiro-tan.org/nlquiz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
