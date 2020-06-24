1d199d4edc1e85cdd7fcf270673719a11b2abc20
Eugene Koontz <ekoontz@hiro-tan.org>
2020-06-25 00:18:24 +0200

    s/ik weet niet/ik weet het niet/

--- src/cljs/nlquiz/quiz.cljs
+++ src/cljs/nlquiz/quiz.cljs
@@ -88,7 +88,7 @@
         [:div
          [:button {:on-click (fn [input-element]
                                (show-possible-answer))
-                   :disabled @ik-weet-niet-button-state} "ik weet niet"]]]
+                   :disabled @ik-weet-niet-button-state} "ik weet het niet"]]]
           
        [:div {:style {:float "left" :width "100%"}}
         [:table
