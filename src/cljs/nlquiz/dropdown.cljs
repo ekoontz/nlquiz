8b8a5da970053d57be257eb198e5855cfe6c8f1e
Eugene Koontz <ekoontz@hiro-tan.org>
2020-06-25 01:01:06 +0200

    accept supplying an 'onchange' when creating the expressions dropdown

--- src/cljs/nlquiz/dropdown.cljs
+++ src/cljs/nlquiz/dropdown.cljs
@@ -7,14 +7,17 @@
    [dag_unify.core :as u]
    [dommy.core :as dommy]))
 
-(defn expressions [expression-chosen-atom]
+(defn expressions [expression-chosen-atom onchange]
   (let [show-these-expressions
         (filter #(= true (u/get-in % [:menuable?] true))
                 nl/expressions)]
      [:select {:id "expressionchooser"
-               :on-change #(reset! expression-chosen-atom
-                                   (js/parseInt
-                                    (dommy/value (dommy/sel1 :#expressionchooser))))}
+               :on-change #(do (reset! expression-chosen-atom
+                                       (js/parseInt
+                                        (dommy/value (dommy/sel1 :#expressionchooser))))
+                               (when onchange
+                                 (log/info (str "doing the onchange that was passed on ...!!"))
+                                 (onchange)))}
       (->>
        (range 0 (count show-these-expressions))
        (map (fn [item-id]
@@ -27,6 +30,7 @@
                      (str (subs (:example expression) 0 15) "..")
                      (:example expression))]))))
        (remove nil?))]))
+
 
 
 
