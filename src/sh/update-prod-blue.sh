e0776c2953ecdfe5222c62d498367af12b41d635
Eugene Koontz <ekoontz@hiro-tan.org>
2020-06-27 13:40:52 +0200

    copy jar to temporary location before replacing

--- src/sh/update-prod-blue.sh
+++ src/sh/update-prod-blue.sh
@@ -1,4 +1,6 @@
 lein uberjar && \
-    scp target/nlquiz.jar hiro-tan.org:nlquiz/target/nlquiz-blue.jar && \
+    scp target/nlquiz.jar hiro-tan.org:nlquiz/target/nlquiz-blue-tmp.jar && \
+    ssh hiro-tan.org "sudo systemctl stop nlquiz-blue" && \
+    ssh hiro-tan.org "mv /home/ekoontz/nlquiz/target/nlquiz-blue-tmp.jar /home/ekoontz/nlquiz/target/nlquiz-blue.jar" && \
     ssh hiro-tan.org "sudo systemctl restart nlquiz-blue"
 
--- src/sh/update-prod-green.sh
+++ src/sh/update-prod-green.sh
@@ -1,4 +1,6 @@
 lein uberjar && \
-    scp target/nlquiz.jar hiro-tan.org:nlquiz/target/nlquiz-green.jar && \
+    scp target/nlquiz.jar hiro-tan.org:nlquiz/target/nlquiz-green-tmp.jar && \
+    ssh hiro-tan.org "sudo systemctl stop nlquiz-green" && \
+    ssh hiro-tan.org "mv /home/ekoontz/nlquiz/target/nlquiz-green-tmp.jar /home/ekoontz/nlquiz/target/nlquiz-green.jar" && \
     ssh hiro-tan.org "sudo systemctl restart nlquiz-green"
 
