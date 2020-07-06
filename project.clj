e8ffd70dd73bf732e833429e4aff95c1f2614cfd
Eugene Koontz <ekoontz@hiro-tan.org>
2020-07-06 09:43:44 +0200

    upgrade to latest reagent release

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring-server "0.5.0"]
                  [prismatic/dommy "1.1.0"]
-                 [reagent "0.9.0-rc4"]
+                 [reagent "0.10.0"]
                  [reagent-utils "0.3.3"]
                  [ring "1.8.0"]
                  [ring/ring-defaults "0.3.2"]
