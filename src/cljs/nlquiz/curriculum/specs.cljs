1eb568bfef3de186e092ad184393d8a14eced800
Eugene Koontz <ekoontz@hiro-tan.org>
2020-07-07 23:59:57 +0200

    +curriculm,specs

---
+++ src/cljs/nlquiz/curriculum/specs.cljs
@@ -0,0 +1,97 @@
+(ns nlquiz.curriculum.specs)
+
+(def curriculum
+  [{:name "Adjectives"
+    :href "adjectives"}
+   {:name "Nouns"
+    :href "nouns"
+    :child [{:name "Definite and indefinite articles"
+             :href "nouns/articles"}
+            {:name "Possessive articles"
+             :href "nouns/poss"}]}])
+
+(def specs
+  [{:note "intensifier adjective"
+    :major-tags ["adjectives"]
+    :example "ongewoon slim"
+    :cat :adjective
+    :mod nil
+    :subcat []
+    :phrasal true
+    :head {:phrasal false}
+    :comp {:phrasal false}}
+   
+   {:note "article+noun"
+    :major-tags ["nouns"]
+    :minor-tags ["articles"]
+    :example "de kat"
+    :cat :noun
+    :subcat []
+    :phrasal true
+    :agr {:number :sing}
+    :head {:phrasal false
+           :subcat {:1 {:cat :det}}}
+    :comp {:phrasal false
+           :sem {:pred :the}}}
+   
+   {:note "article+noun"
+    :major-tags ["nouns"]
+    :minor-tags ["articles"]
+    :example "de kat"
+    :cat :noun
+    :subcat []
+    :phrasal true
+    :agr {:number :plur}
+    :head {:phrasal false
+           :subcat {:1 {:cat :det}}}
+    :comp {:phrasal false
+           :sem {:pred :the}}}
+   
+   {:note "article+noun"
+    :major-tags ["nouns"]
+    :minor-tags ["articles"]
+    :example "de kat"
+    :cat :noun
+    :subcat []
+    :phrasal true
+    :head {:phrasal false
+           :subcat {:1 {:cat :det}}}
+    :comp {:phrasal false
+           :sem {:pred :some}}}
+   
+   {:note "article+noun"
+    :major-tags ["nouns"]
+    :minor-tags ["articles"]
+    :example "de kat"
+    :cat :noun
+    :subcat []
+    :phrasal true
+    :agr {:number :sing}
+    :head {:phrasal false
+           :subcat {:1 {:cat :det}}}
+    :comp {:phrasal false
+           :sem {:pred :some}}}
+   
+   {:major-tags ["nouns"]
+    :minor-tags ["poss"]
+    :example "zijn kat"
+    :cat :noun
+    :subcat []
+    :phrasal true
+    :head {:phrasal false
+           :subcat {:1 {:cat :det}}}
+    :comp {:phrasal false
+           :sem {:pred :he}}}
+   
+   {:major-tags ["nouns"]
+    :minor-tags ["poss"]
+    :example "zijn kat"
+    :cat :noun
+    :subcat []
+    :phrasal true
+    :head {:phrasal false
+           :subcat {:1 {:cat :det}}}
+    :comp {:phrasal false
+           :sem {:pred :she}}}])
+
+
