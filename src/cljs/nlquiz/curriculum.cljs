05e42719c8558e571dc4bca434c4709f61d0e940
Eugene Koontz <ekoontz@hiro-tan.org>
2020-07-14 22:44:27 +0200

    Revert "use nlquiz.core's root-path"
    This reverts commit 0febfb8e588f1e954a5411e0be8eb0933408bf92.

--- src/cljs/nlquiz/curriculum.cljs
+++ src/cljs/nlquiz/curriculum.cljs
@@ -8,12 +8,13 @@
    [cljslog.core :as log]
    [dag_unify.core :as u]
    [dommy.core :as dommy]
-   [nlquiz.core :refer [root-path]]
-   [nlquiz.curriculum.specs :refer [curriculum guides specs]]
+   [nlquiz.curriculum.specs :refer [curriculum specs]]
    [nlquiz.quiz :as quiz]
    [reagent.core :as r])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+;; TODO: move root-path to core:
+(defonce root-path "/nlquiz/")
 (def topic-name (r/atom ""))
 
 ;; this atom is used to add :key values to list items and table rows:
