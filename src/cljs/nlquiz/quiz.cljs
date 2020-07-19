0c74a69cf6404c28fbdcd2a7c0945ba9c5aa61b7
Eugene Koontz <ekoontz@hiro-tan.org>
2020-07-19 12:09:54 +0200

    add 'speak' for each answer table row

--- src/cljs/nlquiz/quiz.cljs
+++ src/cljs/nlquiz/quiz.cljs
@@ -135,6 +135,7 @@
             (map (fn [i]
                    [:tr {:key i :class (if (= 0 (mod i 2)) "even" "odd")}
                     [:th (- @answer-count i)]
+                    [:th.speak [:button {:on-click #(speak-dutch (-> @question-table (nth i) :target))} "🔊"]]
                     [:td.target (-> @question-table (nth i) :target)]
                     [:td.source (-> @question-table (nth i) :source)]
                     ]))))]]]  
