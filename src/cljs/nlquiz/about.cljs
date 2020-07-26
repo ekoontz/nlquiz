a41eb560bd0490a828679fafc112282e3a59f79f
Eugene Koontz <ekoontz@hiro-tan.org>
2020-07-26 16:10:54 +0200

    fix grammar,reword

--- src/cljs/nlquiz/about.cljs
+++ src/cljs/nlquiz/about.cljs
@@ -6,7 +6,7 @@
      [:h3 "About nlquiz"]
      [:p "This is a way to learn some vocabulary and short phrases in Dutch."]
      [:p "Choose a topic in the curriculum to practice with that type of phrase. You'll get English phrases of that type, which you should try to translate to Dutch."]
-     [:p "If you don't know how to translate a phrase, just hit the " [:button.weetniet "Ik weet het niet"] " ('I don't know') button, and you'll be shown the question so that you can try again."]
+     [:p "If you don't know how to translate a phrase, just hit the " [:button.weetniet "Ik weet het niet"] " ('I don't know') button, and you'll be shown a possible translation."]
      [:p "If you see a " [:button.speak {:on-click #(speak/nederlands "de kat")} "🔊"] " button next to a phrase, you can click it to hear the pronunciation of that phrase, for example:"]
      (let [i 0]
        [:div
@@ -20,7 +20,7 @@
            [:td.source "the cat"]]]]])
 
      [:p "You may need to use headphones to hear the sound on mobile devices; I'm not sure yet why this is sometimes required."]
-     [:p "⚠ Caution⚠ There are likely many errors because I am only a beginning at learning Dutch. Not to be used as a substitute for a real class or learning materials."]
+     [:p "⚠ Caution⚠ There are likely many errors because I am only a beginner myself at learning Dutch. Not to be used as a substitute for a real class or learning materials."]
      [:p "The entire software stack is 100% Free Software/Open Source, though with various licenses according to its various origins. To learn more about "
       " the software stack, start " [:a {:href "https://github.com/ekoontz/nlquiz"} "here"] "."]
 
