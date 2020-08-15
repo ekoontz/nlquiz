f7a6763bc0286756ced8dc9f5f0805de93b387e7
Eugene Koontz <ekoontz@hiro-tan.org>
2020-08-15 18:17:48 +0200

    another attempt at model reloading

--- src/clj/nlquiz/server.clj
+++ src/clj/nlquiz/server.clj
@@ -9,19 +9,35 @@
     (:gen-class))
 
 (defn initialize-language-models []
-  (log/info (str "initializing.."))
-  (log/info (str "loading nl model.."))
-  (nl/load-model)
-  (log/info (str "loaded."))
-  (log/info (str "loading en model.."))
-  (en/load-model)
-  (log/info (str "loaded.")))
+   (log/info (str "It's time for the reloading-of-the-models.."))
+   (log/info (str "loading nl model.."))
+   (let [new-model (nl/create-model)]
+     (log/info (str "model loaded; replacing."))    
+     (let [created-model (nl/create-model)]
+       (dosync
+        (ref-set nl/model created-model))))
+   (log/info (str "loaded."))
+   (log/info (str "loading en model.."))
+   (let [new-model (en/create-model)]
+     (log/info (str "model loaded; replacing."))
+     (let [created-model (en/create-model)]
+       (dosync
+        (ref-set en/model created-model))))
+   (log/info (str "loaded."))
+   (log/info (str "That's all for the reloading-of-the-models 'til next time!")))
 
 (use 'ruiyun.tools.timer)
+;; TODO: check if we got here are running as 'lein uberjar' here somehow (?)
+;; or in other words, only run this task if we're running the the -main of the jar.
 (if (System/getenv "MODEL_URL")
   (run-task! initialize-language-models
-             :period (* 5 60 1000)) ;; reload every 5 minutes.
-  (log/warn (str "MODEL_URL was not defined in the environment: will not be able to refresh language models. In the future, please set your environment's MODEL_URL to 'file:///Users/ekoontz/menard/src/'")))
+             :period (* 1 60 1000)) ;; reload every 5 minutes.
+  (log/warn (str "MODEL_URL was not defined in the environment: will not be able to "
+                 "refresh language models. In the future, please set "
+                 "your environment's MODEL_URL to 'file:///Users/ekoontz/menard/src/', "
+                 "unless you are seeing this warning when you running 'lein uberjar', in which "
+                 "case, you can safely ignore this warning since it's not relevant.")))
+                 
 
 (defn -main [& args]
   (let [port (or (env :port) 3000)]
