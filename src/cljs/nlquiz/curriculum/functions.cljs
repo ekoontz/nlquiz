ee95d59962b2039e817dde0a2ceb9ac7a47d58aa
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-12 14:48:37 +0200

    generate 3 examples instead of 2

--- src/cljs/nlquiz/curriculum/functions.cljs
+++ src/cljs/nlquiz/curriculum/functions.cljs
@@ -7,7 +7,7 @@
             [reagent.core :as r])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def this-many-examples 2)
+(def this-many-examples 3)
 
 (defn new-pair [spec]
   (let [input (r/atom nil)
