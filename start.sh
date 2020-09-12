47d054f008c2bcb7b452dfafee47e90c1077ba0b
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-13 00:13:53 +0200

    don't compile menard; do 'lein clean'

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-(cd ../menard ; git checkout master; make clean; make compile; lein install);
+#(cd ../menard ; git checkout master; make clean; make compile; lein install);
+lein clean
 rm target/cljsbuild/public/js/app.js target/cljsbuild/public/js/app-optimized.js
 lein cljsbuild once min
 lein figwheel
