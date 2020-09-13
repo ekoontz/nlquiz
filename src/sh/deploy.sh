b9f10de8894a44b648b98034dfeb218a7d95f6d0
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-13 12:09:47 +0200

    deploy script: checkout current source-dir's master

--- src/sh/deploy.sh
+++ src/sh/deploy.sh
@@ -1,7 +1,7 @@
 ( stat /tmp/nlquiz/ || (git clone $(pwd) /tmp/nlquiz ) && \
       cd /tmp/nlquiz && \
       lein clean && git config pull.ff only && \
-      git pull origin master && ./src/sh/update-prod-blue.sh && \
+      git fetch origin && git checkout origin/master && ./src/sh/update-prod-blue.sh && \
       sleep 60 && ./src/sh/blue-to-green.sh && \
       ./src/sh/pull.sh
 )
