f2be3b0ea788f3f9b942b3ea372f40cbc9a7dea6
Eugene Koontz <ekoontz@hiro-tan.org>
2020-11-07 15:18:14 +0100

    improve wording

--- src/cljs/nlquiz/curriculum/content.cljs
+++ src/cljs/nlquiz/curriculum/content.cljs
@@ -245,7 +245,7 @@
     "numbers"
     (fn []
       [:div
-       [:p "In English, when we want to say a number between 19 and 100, we give the base-10 word: 'twenty', 'seventy', etc, and then the base-1 word: the 'four' in 'twenty four'. But in Dutch, the word order is reversed, so 'twenty four' is " [:i "vier en twintig"] "."]
+       [:p "In English, a number like 'twenty four' is expressed first with the base-10 word: 'twenty', and " [:i "then"] " the base-1 word: 'four'. But in Dutch, the word order is reversed, so 'twenty four' is " [:i "vier en twintig"] "."]
        [show-examples
         [{:example "de vier kliene vogels"
           :cat :noun
