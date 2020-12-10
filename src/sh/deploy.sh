14f77df058524871e6098e5e3d73141890f3bf74
Eugene Koontz <ekoontz@hiro-tan.org>
2020-12-10 19:06:38 +0100

    deploy script update

--- src/sh/deploy.sh
+++ src/sh/deploy.sh
@@ -1,6 +1,6 @@
 ( stat /tmp/nlquiz/ || (git clone $(pwd) /tmp/nlquiz ) && \
       cd /tmp/nlquiz && \
-      lein clean && git config pull.ff only && \
+      git checkout master && lein clean && git config pull.ff only && \
       git fetch origin && git checkout origin/master && ./src/sh/update-prod-blue.sh && \
       sleep 60 && ./src/sh/blue-to-green.sh && \
       ./src/sh/pull.sh
