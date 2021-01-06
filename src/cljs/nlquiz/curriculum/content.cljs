9d0b4349a86cc6f89637f8dfb10cfacf32889b2a
Eugene Koontz <ekoontz@hiro-tan.org>
2021-01-06 23:33:45 +0100

    add 'demonstrative' course material

--- src/cljs/nlquiz/curriculum/content.cljs
+++ src/cljs/nlquiz/curriculum/content.cljs
@@ -70,6 +70,27 @@
                   :number :sing}
             :phrasal true
             :head {:phrasal false}}]]]]])
+
+    "demonstratives"
+    (fn []
+      [:div
+       [:p "There are three demonstrative articles:"]
+       [:ul
+        [:li [:i "dat"] "- used for 'that' with " [:b "neuter"] " nouns"]
+        [:li [:i "deze"] "- used for 'this' with " [:b "common"] " nouns, and for 'these' with all nouns"]
+        [:li [:i "die"] "- used for 'that' with " [:b "common"] " nouns, and for 'those' with all nouns"]]
+       [show-examples
+        [{:cat :noun
+          :subcat []
+          :phrasal true
+          :comp {:phrasal false
+                 :sem {:pred :that}}}
+         {:cat :noun
+          :subcat []
+          :phrasal true
+          :comp {:phrasal false
+                 :sem {:pred :that}}}]]])
+    
     "poss" 
     (fn []
       [:div
