2ae1e1a380f8d976a11618845c22f0748b8fea16
Eugene Koontz <ekoontz@hiro-tan.org>
2021-01-07 09:14:21 +0100

    move <li>s around for clarity

--- src/cljs/nlquiz/curriculum/content.cljs
+++ src/cljs/nlquiz/curriculum/content.cljs
@@ -77,8 +77,9 @@
        [:p "There are three demonstrative articles:"]
        [:ul
         [:li [:i "deze"] " - used for 'this' with " [:b "common"] " nouns, and for 'these' with all nouns"]
-        [:li [:i "dat"] " - used for 'that' with " [:b "neuter"] " nouns"]
-        [:li [:i "die"] " - used for 'that' with " [:b "common"] " nouns, and for 'those' with all nouns"]]
+        [:li [:i "die"] " - used for 'that' with " [:b "common"] " nouns, and for 'those' with all nouns"]
+        [:li [:i "dat"] " - used for 'that' with " [:b "neuter"] " nouns"]]
+
        [show-examples
         [{:cat :noun
           :subcat []
