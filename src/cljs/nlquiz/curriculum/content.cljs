b00fbdade621fe18b1b40f09b2f4158f9d72156a
Eugene Koontz <ekoontz@hiro-tan.org>
2021-01-07 09:03:30 +0100

    change order of list items for clarity

--- src/cljs/nlquiz/curriculum/content.cljs
+++ src/cljs/nlquiz/curriculum/content.cljs
@@ -76,8 +76,8 @@
       [:div
        [:p "There are three demonstrative articles:"]
        [:ul
-        [:li [:i "dat"] " - used for 'that' with " [:b "neuter"] " nouns"]
         [:li [:i "deze"] " - used for 'this' with " [:b "common"] " nouns, and for 'these' with all nouns"]
+        [:li [:i "dat"] " - used for 'that' with " [:b "neuter"] " nouns"]
         [:li [:i "die"] " - used for 'that' with " [:b "common"] " nouns, and for 'those' with all nouns"]]
        [show-examples
         [{:cat :noun
