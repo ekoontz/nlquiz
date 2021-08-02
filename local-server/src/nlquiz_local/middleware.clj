40f6a2506d6d8e1b890ee6d4c56ac0009c9e4732
Eugene Koontz <ekoontz@hiro-tan.org>
2021-08-03 00:50:24 +0200

    cleanup: use menard's server instead of the one here

--- local-server/src/nlquiz_local/middleware.clj
+++
@@ -1,7 +0,0 @@
-(ns nlquiz-local.middleware
-  (:require
-   [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
-
-(def middleware
-  [#(wrap-defaults % (assoc site-defaults :session false))])
-
--- src/sh/local.sh
+++ src/sh/local.sh
@@ -31,10 +31,11 @@ lein clean
 # The ORIGIN is the URL for the UI server (started below).
 # The language endpoint needs to know that URL so it can set the
 # header: 'Access-Control-Allow-Origin' to that.
-cd local-server
+pushd .
+cd ~/menard/server
 ORIGIN=http://${IP}:3449 lein ring server-headless &
 LANGUAGE_SERVER_PID=$!
-cd ..
+popd
 # Start the UI server. It needs to know the URL for the language endpoint server
 # so it can tell the client to use that URL for generating guesses and parse answers:
 echo LANGUAGE_ENDPOINT_URL=http://${IP}:3000 ROOT_PATH=http://${IP}:3449/ lein figwheel
