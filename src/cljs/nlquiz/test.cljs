97de2288fb5f0a3d8f8341f8321647fb01284f16
Eugene Koontz <ekoontz@hiro-tan.org>
2021-08-22 23:59:01 +0200

    decoding parse works

--- src/cljs/nlquiz/test.cljs
+++ src/cljs/nlquiz/test.cljs
@@ -17,18 +17,21 @@
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [nlquiz.handler :refer [root-path-from-env inline-resource language-server-endpoint-url]]))
 
-(def tokens (r/atom spinner))
-(def grammar (r/atom spinner))
-(def span-pairs (r/atom spinner))
+(def tokens-a (r/atom spinner))
+(def grammar-a (r/atom spinner))
+(def span-pairs-a (r/atom spinner))
 
-(defn decode-tokens [response-body]
-  ;; a map between:
-  ;; keys: each key is a span e.g. [0 1]
-  ;; vals: each val is a sequence of serialized lexemes
-  (into {}
-        (->> (keys response-body)
-             (map (fn [k]
-                    [k (map deserialize (get response-body k))])))))
+(defn decode-parse [response-body]
+   ;; a map between:
+   ;; keys: each key is a span e.g. [0 1]
+   ;; vals: each val is a sequence of serialized lexemes
+   (into {}
+         (->> (keys response-body)
+              (map (fn [k]
+                     [k
+                      (map (fn [serialized-lexeme]
+                             (-> serialized-lexeme cljs.reader/read-string deserialize))
+                           (get response-body k))])))))
 
 (defn decode-grammar [response-body]
   (map deserialize response-body))
@@ -37,50 +40,43 @@
   (go (let [parse-response (<! (http/get (str (language-server-endpoint-url)
                                               "/parse-start?q=" "de grote pinda")))
             grammar-response (<! (http/get (str (language-server-endpoint-url)
-                                                "/grammar/nl")))]
-        (let [grammar-decoded (-> grammar-response :body decode-grammar)
-              tokens-decoded (-> parse-response :body decode-tokens)]
-          (reset! grammar grammar-decoded)
-          (reset! tokens tokens-decoded)
-          (reset! span-pairs (parse/span-pairs (-> tokens-decoded keys count) 2)))))
+                                                "/grammar/nl")))
+
+;;            next-stage (binding [parse/syntax-tree (fn [x] "test:syntax-tree:type:" (type x))]
+;;                         (parse/parse-next-stage input-map input-length 2 grammar))
+            ]
+        (let [grammar (-> grammar-response :body decode-grammar)]
+          (reset! grammar-a grammar)
+          (reset! span-pairs-a span-pairs))
+        (log/info (str "PARSE RESPONSE: " parse-response))
+        (let [parse (-> parse-response :body decode-parse)]
+          (log/info (str "DECODED PARSE RESPONSE: " parse)))))
+        
   (fn []
     [:div
-
+     (comment
      [:div.debug
       [:h2 "tokens"]
-      (if (map? @tokens)
+      (if (map? @tokens-a)
         [:table
          [:thead
           [:tr [:th "span"] [:th "tokens"]]]
          [:tbody
-          (->> (keys @tokens)
+          (->> (keys @tokens-a)
                (map (fn [k]
                       [:tr {:key (str k)}
                        [:td k]
-                       [:td (get @tokens k)]]))
-               doall)]])]
+                       [:td (get @tokens-a k)]]))
+               doall)]])])
+
      [:div.debug
       [:h2 "grammar"]
-      (if (seq @grammar)
-                   (->> @grammar
-                        (map dag_unify.core/pprint)
-                        (map (fn [rule]
-                               [:div.debug {:key (md5/string->md5-hex (str rule))} (str rule)]))))]
+      (if (seq @grammar-a)
+        (->> @grammar-a
+             (map dag_unify.core/pprint)
+             (map (fn [rule]
+                    [:div.debug {:key (md5/string->md5-hex (str rule))} (str rule)]))))]
      [:div.debug
-      [:h2 "span pairs"]
-      (str @span-pairs)]
+      [:h2 "span pairs.."]
+      (str @span-pairs-a)]
      ]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
