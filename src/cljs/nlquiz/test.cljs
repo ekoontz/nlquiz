f814b77aaab2037732e70a7a89582704ceefc070
Eugene Koontz <ekoontz@hiro-tan.org>
2021-08-23 09:02:05 +0200

    able to parse one step

--- src/cljs/nlquiz/test.cljs
+++ src/cljs/nlquiz/test.cljs
@@ -28,30 +28,35 @@
    (into {}
          (->> (keys response-body)
               (map (fn [k]
-                     [k
+                     (log/info (str "K: " (cljs.reader/read-string (clojure.string/join "" (rest (str k))))))
+                     [(cljs.reader/read-string (clojure.string/join "" (rest (str k))))
                       (map (fn [serialized-lexeme]
                              (-> serialized-lexeme cljs.reader/read-string deserialize))
                            (get response-body k))])))))
 
 (defn decode-grammar [response-body]
-  (map deserialize response-body))
+  (map (fn [rule] (-> rule cljs.reader/read-string deserialize))
+       response-body))
 
 (defn test []
   (go (let [parse-response (<! (http/get (str (language-server-endpoint-url)
                                               "/parse-start?q=" "de grote pinda")))
             grammar-response (<! (http/get (str (language-server-endpoint-url)
-                                                "/grammar/nl")))
-
-;;            next-stage (binding [parse/syntax-tree (fn [x] "test:syntax-tree:type:" (type x))]
-;;                         (parse/parse-next-stage input-map input-length 2 grammar))
-            ]
-        (let [grammar (-> grammar-response :body decode-grammar)]
-          (reset! grammar-a grammar)
-          (reset! span-pairs-a span-pairs))
-        (log/info (str "PARSE RESPONSE: " parse-response))
-        (let [parse (-> parse-response :body decode-parse)]
-          (log/info (str "DECODED PARSE RESPONSE: " parse)))))
-        
+                                                "/grammar/nl")))]
+        (let [grammar (-> grammar-response :body decode-grammar)
+              input-map (-> parse-response :body decode-parse)
+              input-length (count (keys input-map))]
+          (log/info (str "TEST (get [0 1]: "
+                         (-> input-map (get
+                                        [0 1]))))
+          (let [next-stage (binding [parse/syntax-tree (fn [x] "test:syntax-tree:"
+                                                         (or (u/get-in x [:rule])
+                                                             (u/get-in x [:canonical])))]
+                             (log/info (str "INPUT-LENGTH: " input-length))
+                             (log/info (str "GRAMMAR: " grammar))                     
+                             (parse/parse-next-stage input-map input-length 2 grammar))]
+            (log/info (str "next-stage: " (keys next-stage)))
+            (reset! grammar-a grammar)))))
   (fn []
     [:div
      (comment
