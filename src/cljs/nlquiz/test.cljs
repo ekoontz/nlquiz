7ba3eb9b24ad56b4105764dad35529d87fb5544c
Eugene Koontz <ekoontz@hiro-tan.org>
2021-10-11 09:02:03 +0200

    improve comments

--- src/cljs/nlquiz/test.cljs
+++ src/cljs/nlquiz/test.cljs
@@ -17,28 +17,29 @@
     (let [nl-surface (-> input-element .-target .-value string/trim)
           fresh? (fn [] (= @nl-surface-atom nl-surface))]
       (when (not (fresh?))
-        ;; only start the (go) if there is a difference between the input we are given (nl-surface)
+        ;; Only start the (go) if there is a difference between the input we are given (nl-surface)
         ;; and the last input that was processed (@nl-surface-atom).
 
-        ;; change english output to spinner since it will be updated, if not by this (go), then by a subsequent (go):
+        ;; Change english output to spinner since it will be updated, if not by this (go), then by a subsequent (go):
         (reset! en-surfaces-atom spinner)
 
         (go
           (reset! nl-surface-atom nl-surface)
 
-          ;; get the information necessary from the server about the NL expression to start parsing on the client side:
+          ;; 1. Get the information necessary from the server about the NL expression to start parsing on the client side:
           (let [parse-response (-> (<! (http/get (str (language-server-endpoint-url)
                                                       "/parse-start?q=" nl-surface)))
                                    :body decode-parse)]
             (when (fresh?)
 
-              ;; With this information ready, now do the parsing, and for that set of parses,
-              ;; get the equivalent set of specifications for the english:
+              ;; 2. With this information ready, now do the NL parsing:
               (let [nl-parses (nl-parses parse-response @grammar nl-surface)
+                    ;; And for that set of NL parses, get the equivalent
+                    ;; set of specifications for the english:
                     en-specs (nl-parses-to-en-specs nl-parses)]
 
-                ;; generate an english expression for each spec in en-specs.
-                ;; for each generated expression, add it to the 'update-to' atom.
+                ;; 3. For each such spec, generate an english expression, and
+                ;;    for each generated expression, add it to the 'update-to' atom.
                 (let [update-to (atom [])]
                   (doseq [en-spec en-specs]
                     (when (fresh?)
@@ -50,7 +51,7 @@
                                                       @update-to)
                                                 set
                                                 vec))))))
-                  ;; update the english UI element with a common-delimited string of all of
+                  ;; 4. Update the english UI element with a common-delimited string of all of
                   ;; the members of the 'update-to' atom:
                   (when (fresh?)
                     (reset! en-surfaces-atom (if (seq @update-to)
