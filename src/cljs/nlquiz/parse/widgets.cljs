42701ddec66a523d15b3a860eb070d090802b8ae
Eugene Koontz <ekoontz@hiro-tan.org>
2021-10-31 11:54:54 +0100

    working on passing up x and y from right child

--- src/cljs/nlquiz/parse/widgets.cljs
+++ src/cljs/nlquiz/parse/widgets.cljs
@@ -39,7 +39,10 @@
         right-canonical (u/get-in tree [:2 :canonical])
         right-show (or right-rule right-surface right-canonical)
         parent        {:x (* x       h-unit) :y (+ vspace (* y       v-unit))}
-        left-child    {:x (* (- x 1) h-unit) :y (+ vspace (* (+ y 1) v-unit))}
+        left-child-coordinates {:x (- x 1)
+                                :y (+ y 1)}
+        left-child    {:x (* (:x left-child-coordinates) h-unit)
+                       :y (+ vspace (* (:y left-child-coordinates) v-unit))}
         parent-class "rule"
         left-class   (if left-rule "rule" "leaf")
         right-class  (if right-rule "rule" "leaf")
@@ -47,7 +50,9 @@
         left-node
         (if left-rule
           (draw-node (u/get-in tree [:1]) (- x 1) (+ y 1))
-          {:g [:text       {:class left-class
+          {;;:x (:x left-child-coordinates)
+           ;;:y (:y left-child-coordinates)
+           :g [:text       {:class left-class
                             :x (:x left-child)
                             :y (+ vspace (:y left-child))} left-show]})
         left-contents (:g left-node)
@@ -58,33 +63,46 @@
         debug (if (:rule left-node)
                 (log/info (str "LEFT X: " left-x "; LEFT Y: " left-y "; RULE: " left-rule)))
 
-        right-child   {:x (* (+ x 1) h-unit) :y (+ vspace (* (+ y 1) v-unit))}
+        right-x-increment (Math/max 1 left-x)
+        right-y-increment (Math/max 1 left-y)
+        right-child-coordinates {:x (+ x right-x-increment)
+                                 :y (+ y right-y-increment)}
+        right-child {:x (* (:x right-child-coordinates) h-unit)
+                     :y (+ vspace (* (:y right-child-coordinates) v-unit))}
         
         right-contents
         (if right-rule
-          (:g (draw-node (u/get-in tree [:2]) (+ x 1) (+ y 1)))
-          [:text       {:class right-class
-                        :x (:x right-child)
-                        :y (+ vspace (:y right-child))} right-show])]
-      {:x (or (:x left-node) x)
-       :y (or (:y left-node) y)
-       :rule (u/get-in tree [:rule])
-       :g
-       [:g
-        [:text {:class parent-class
-                :x (:x parent)
-                :y (:y parent)}
-         show]
-        [:line.thick {:x1 (:x parent) :y1 (:y parent) :x2 (:x left-child)  :y2 (:y left-child)}]
-        [:line.thick {:x1 (:x parent) :y1 (:y parent) :x2 (:x right-child) :y2 (:y right-child)}]
-        
-        left-contents
-        right-contents]}))
+          (do
+            (log/info (str "ok, let's draw the right rule: " right-rule
+                           " with right-coords: " right-child-coordinates))
+                           
+            (:g (draw-node (u/get-in tree [:2])
+                           (:x right-child-coordinates)
+                           (:y right-child-coordinates))))
+          (do
+            (log/info (str "ok let's draw the right child leaf: " right-show
+                           " with right-coords: " right-child-coordinates))
+            [:text       {:class right-class
+                          :x (:x right-child)
+                          :y (+ vspace (:y right-child))} right-show]))]
+    (log/info (str "returning from rule: " (u/get-in tree [:rule])
+                   " with right-child-coordinates: " right-child-coordinates))
+    {:x (or (:x right-child-coordinates) x)
+     :y (or (:y right-child-coordinates) y)
+     :rule (u/get-in tree [:rule])
+     :g
+     [:g
+      [:text {:class parent-class
+              :x (:x parent)
+              :y (:y parent)}
+       show]
+      [:line.thick {:x1 (:x parent) :y1 (:y parent) :x2 (:x left-child)  :y2 (:y left-child)}]
+      [:line.thick {:x1 (:x parent) :y1 (:y parent) :x2 (:x right-child) :y2 (:y right-child)}]
+      
+      left-contents
+      right-contents]}))
 
 (defn draw-tree [tree]
-  (if tree
-    (log/info (str "drawing tree.."))
-    (log/info (str "er is nog geen tree..?")))
   (if (u/get-in tree [:rule])
     [:svg
      (:g (draw-node tree 2 0))]))
