47e5d2251fffff28e571d333a05fc44c0d4d194c
Eugene Koontz <ekoontz@hiro-tan.org>
2021-11-07 21:02:37 +0100

    fix last partial commit

--- src/cljs/nlquiz/parse.cljs
+++ src/cljs/nlquiz/parse.cljs
@@ -49,12 +49,12 @@
                               ;; unless language-models-loaded? is true,
                               ;; can't parse user's guess:
                               :on-change (when language-models-loaded?
-                                           (on-change nl-surface-atom nl-tree-atom en-surfaces-atom grammar))}]]
                                            (on-change nl-surface-atom
                                                       nl-tree-atom
                                                       en-surfaces-atom
                                                       grammar))}]]
          (nl-widget nl-surface-atom nl-tree-atom nl-node-html-atom)]))))
+
 
 
 
