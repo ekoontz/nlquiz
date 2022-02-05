ade042ad6ba75285c8d3da14df90934a7cc7ba00
Eugene Koontz <ekoontz@hiro-tan.org>
2022-02-05 22:54:53 +0100

    improve use of flags

--- src/cljs/nlquiz/parse/widgets.cljs
+++ src/cljs/nlquiz/parse/widgets.cljs
@@ -2,14 +2,12 @@
 
 (defn en-widget [trees lexemes rules]
   [:div.en_widget
-   "🇬🇧 "
    @trees
    @lexemes
    @rules])
 
 (defn nl-widget [trees lexemes rules]
   [:div.nl_widget
-   "🇳🇱 "
    @trees
    @lexemes
    @rules])
