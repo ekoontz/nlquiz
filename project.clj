cd57be906f5c58733e448a7e0129bf44ea2a8eb3
Eugene Koontz <ekoontz@hiro-tan.org>
2022-03-06 17:04:53 +0100

    adding timer support to avoid excessive linguistics

--- project.clj
+++ project.clj
@@ -24,6 +24,7 @@
                  [org.clojure/tools.logging "1.1.0"]
                  [dag_unify "1.10.1"]
                  [menard "1.4.5"]
+                 [io.helins/timer.cljs "2.0.0"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-devel "1.7.1"]
--- src/cljs/nlquiz/quiz.cljs
+++ src/cljs/nlquiz/quiz.cljs
@@ -6,6 +6,7 @@
    [dag_unify.core :as u]
    [dag_unify.diagnostics :as d]
    [dag_unify.serialization :refer [deserialize serialize]]
+   [helins.timer :as timer]
    [menard.parse :as parse]
    [menard.translate.spec :as tr]
    [nlquiz.constants :refer [spinner]]
@@ -142,12 +143,14 @@
 (def grammar (atom nil))
 (def morphology (atom nil))
 
-(defn submit-guess [guess-text the-input-element parse-html possible-correct-semantics if-correct-fn nl-parses-atom]
+(defn submit-guess [guess-text the-input-element
+                    parse-html possible-correct-semantics
+                    if-correct-fn nl-parses-atom]
+  (log/info (str "submit-guess: input: " the-input-element))
   (if (empty? @possible-correct-semantics)
     (log/error (str "there are no correct answers for this question.")))
-  (reset! guess-text the-input-element)
-  (let [guess-string @guess-text]
-    (log/debug (str "submitting your guess: " guess-string))
+  (let [guess-string the-input-element]
+    (log/info (str "submitting your guess: " guess-string))
     (reset! translation-of-guess spinner)
     (go (let [parse-response
               (->
@@ -180,21 +183,21 @@
           ;; @not-answered-yet? *was* true when we fired off the request, but might not be true anymore,
           ;; if the user correctly answered this question, and another guess is being submitted
           ;; because they're still typing after that, so prevent re-evaluation in that case.
-          (when (not (= guess-string @guess-text))
-            (log/debug (str "guess-text changed: it's now: '" @guess-text "', so "
-                           "we won't bother updating with responding to older "
-                           "user guess: '" guess-string "'.")))
-          (when (and @not-answered-yet?
-                     (= guess-string @guess-text))
-            (log/debug (str "*LOCAL* semantics of guess: " local-sem))
-            (if (evaluate-guess local-sem
-                                @possible-correct-semantics)
-              ;; got it right!
-              (if-correct-fn guess-string)
-
-              ;; got it wrong:
-              (do (log/debug (str "sorry, your guess: '" guess-string "' was not right.")))))))))
-
+          (if (or false
+                  (and @not-answered-yet?
+                       (= guess-string @guess-text)))
+            (do
+              (log/debug (str "*LOCAL* semantics of guess: " local-sem))
+              (if (evaluate-guess local-sem
+                                  @possible-correct-semantics)
+                ;; got it right!
+                (if-correct-fn guess-string)
+                
+                ;; got it wrong:
+                (do (log/debug (str "sorry, your guess: '" guess-string "' was not right.")))))
+            (do
+              (log/info (str "not updating with any response to guess-string: " guess-string))))))))
+  
 (defn load-linguistics []
   (go
     (let [grammar-response (<! (http/get (str (language-server-endpoint-url)
@@ -203,9 +206,12 @@
                                                  "/morphology/nl")))]
       (reset! grammar (-> grammar-response :body decode-grammar))
       (reset! morphology (-> morphology-response :body decode-morphology)))))
-  
+
+(def timer (r/atom (.getTime (js/Date.))))
+
 (defn quiz-layout []
   [:div.main
+   [:div#timer @timer]
    [:div#answer {:style {:display @show-answer-display}} @show-answer]
    [:div#praise {:style {:display @show-praise-display}} @show-praise-text]
    [:div.question-and-guess
@@ -225,33 +231,43 @@
                 :value @guess-text
                 :disabled @input-state
                 :on-change (fn [input-element]
-                             (when @not-answered-yet?
-                               (reset! input-state "disabled")
-                               (reset! not-answered-yet? false)                               
-                               (log/debug (str "current guess size: " (-> input-element .-target .-value count)))
-                               (reset! guess-input-size (max initial-guess-input-size (+ 0 (-> input-element .-target .-value count))))
-                               (submit-guess guess-text
-                                             (-> input-element .-target .-value)
-                                             parse-html
-                                             possible-correct-semantics
-                                             ;; function called if the user guessed correctly:
-                                             (fn [correct-answer]
-                                               (.focus (.getElementById js/document "other-input"))
-                                               (reset! guess-text "")
-                                               (reset! not-answered-yet? false)
-                                               (reset! got-it-right? true)
-                                               (reset! show-answer correct-answer)
-                                               (reset! translation-of-guess "")
-                                               (if (.-requestSubmit (.getElementById js/document "quiz"))
-                                                 (.requestSubmit (.getElementById js/document "quiz"))
-                                                 (.dispatchEvent (.getElementById js/document "quiz")
-                                                                 (new js/Event "submit" {:cancelable true})))
-                                               (.focus (.getElementById js/document "input-guess"))
-                                               (reset! show-answer correct-answer)
-                                               nl-parses-atom))
-                               (reset! not-answered-yet? true)
-                               (reset! input-state "")))}]]] ;; div.guess
-
+                             (let [old-timer-value @timer]
+                               (reset! timer (.getTime (js/Date.)))
+                               (log/info (str "TIME SINCE LAST KEYSTROKE: " (- @timer old-timer-value) "; currently: '" (-> input-element .-target .-value) "'"))
+                               (when @not-answered-yet?
+                                 (reset! input-state "disabled")
+                                 (reset! not-answered-yet? false)                               
+                                 (log/debug (str "current guess size: " (-> input-element .-target .-value count)))
+                                 (reset! guess-input-size (max initial-guess-input-size (+ 0 (-> input-element .-target .-value count))))
+                                 (if (> (- @timer old-timer-value) 200)
+                                   (do
+                                     (log/info (str "it's been long enough to try parsing a new guess: " (-> input-element .-target .-value)))
+                                     (submit-guess guess-text
+                                                   (-> input-element .-target .-value)
+                                                   parse-html
+                                                   possible-correct-semantics
+                                                   ;; function called if the user guessed correctly:
+                                                   (fn [correct-answer]
+                                                     (.focus (.getElementById js/document "other-input"))
+                                                     (reset! guess-text "")
+                                                     (reset! not-answered-yet? false)
+                                                     (reset! got-it-right? true)
+                                                     (reset! show-answer correct-answer)
+                                                     (reset! translation-of-guess "")
+                                                     (if (.-requestSubmit (.getElementById js/document "quiz"))
+                                                       (.requestSubmit (.getElementById js/document "quiz"))
+                                                       (.dispatchEvent (.getElementById js/document "quiz")
+                                                                       (new js/Event "submit" {:cancelable true})))
+                                                     (.focus (.getElementById js/document "input-guess"))
+                                                     (reset! show-answer correct-answer)
+                                                     nl-parses-atom))))
+                                 (reset! guess-text (-> input-element .-target .-value))
+                                 (reset! not-answered-yet? true)
+                                 (reset! input-state "")
+                                 (.focus (.getElementById js/document "input-guess"))
+                                 )))
+                }]]] ;; /div.guess
+      
      [:div.english @translation-of-guess]
 
      [:div.dontknow
@@ -362,6 +378,9 @@
               (reset! input-state "")
               (.focus (.getElementById js/document "input-guess"))))))))
 
+(defn check-user-input []
+  (log/info (str "checking user input which is currently: " @guess-text)))
+
 (defn quiz-component []
   (load-linguistics)
   (let [routing-data (session/get :route)
@@ -371,6 +390,15 @@
     (reset! major-atom major)
     (reset! minor-atom minor)
     (reset! question-html spinner)
+    (if true
+    (timer/every timer/main-thread
+                 2000
+                 check-user-input
+                 (fn on-lag [delta]
+                   ;; Optional
+                   (log/info (str "some lag found: delta=" delta))
+                   )))
+    
     (get-expression major minor)
     (fn []
       [:div.curr-major
