c9870a61dbe6ab32516cab70e8843bc86d413a3f
Eugene Koontz <ekoontz@hiro-tan.org>
2022-08-21 18:36:13 +0200

    use latest release of dependency 'dag_unify'

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                  [clj-http "3.12.3"]
                  [cljs-http "0.1.46"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [dag_unify "1.10.3-SNAPSHOT"]
+                 [dag_unify "1.10.3"]
                  [org.clojure/tools.logging "1.2.4"]
                  [menard "1.5.0"]
                  [ring/ring-core "1.7.1"]
